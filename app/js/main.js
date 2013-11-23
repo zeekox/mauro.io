70ea9bb3a5cd88700dbf1912b074e8333b978f15
Mauro Carabotti <zeekox@gmail.com>
2013-11-23 20:35:48 +0100

    fire link when clicking on list item

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,19 @@
 
 			var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
 
-			$('img').each(function(index) {
+			$('img').each(function() {
 				var img_url = $(this).attr('src').replace(/\d\d$/, $(this).data(size));
 				$(this).attr('src', img_url);
+			});
+
+			$('nav li').on('click', function(e) {
+				var li = $(this);
+
+				if ( !$(e.target).is('a') ){
+					var link = $('a', li);
+					window.location = link.attr('href');
+				}
+
 			});
 
 		});
