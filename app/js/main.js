148bbc75fec420bb9dd6db5e5128cfb0bdc6268b
Mauro Carabotti <zeekox@gmail.com>
2013-11-29 22:48:01 +0100

    fix js to work in firefox

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,17 @@
 
 			var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
 
-			$('img').each(function() {
-				var img_url = $(this).attr('src').replace(/\d\d$/, $(this).data(size));
-				$(this).attr('src', img_url);
-			});
+			if(size) {
+				$('img').each(function() {
+					// firefox put property value between "
+					var size_attr = $(this).data(size.replace(/"/g, ''));
+
+					if(size_attr) {
+						var img_url = $(this).attr('src').replace(/\d\d$/, size_attr);
+						$(this).attr('src', img_url);
+					}
+				});
+			}
 
 			$('li').click(function(e) {
 				var li = $(this);
