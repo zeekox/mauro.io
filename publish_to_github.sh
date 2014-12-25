56ce82c6fe88239842dc60c8122ef5b01df8da75
Mauro Carabotti <zeekox@gmail.com>
2014-12-25 15:14:29 +0100

    comment line to delete gh-pages branch

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -1,2 +1,3 @@
-git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
+# to delete the remote branch when it cannot be pushed (rejected)
+#git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
 git subtree push --prefix dist origin gh-pages
