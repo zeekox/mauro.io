8605cbecb8d9e26c6d47dcd003fa04620c4b1592
Mauro Carabotti <carabotti@puzzle.ch>
2016-01-27 08:24:56 -0500

    no need for script, now made with grunt publish

--- publish_to_github.sh
+++
@@ -1,3 +0,0 @@
-# to delete the remote branch when it cannot be pushed (rejected)
-#git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
-git subtree push --prefix dist origin gh-pages
